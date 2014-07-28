7e6753d700e42f234621e7dc99746eda8c26bcee
Kayla <KaylaLombardoni@Kaylas-MacBook-Pro.local>
2014-07-28 18:31:11 -0400

    first commit

---
+++ temperature.rb
@@ -0,0 +1,37 @@
+# todays_temperature = 92
+
+# if todays_temperature > 90
+
+# 	puts " Im going hicking"
+
+# end
+
+# --------
+
+# if 10 == 5+5 
+
+# 	puts "ten is still ten! All right!"
+
+# end 
+
+# ------------
+
+
+
+
+def going_hiking (temp) 
+
+if temp >= 50
+
+	puts "#{temp} is perfect for hiking"
+
+
+
+elsif temp < 50
+
+	puts "#{temp} is to cold for hiking"
+end
+
+end 
+
+going_hiking(30)
---
+++ todays_temperature.rb
@@ -0,0 +1,7 @@
+# todays_temperature = 23
+#  if todays_temperature > 50
+#  	puts"im going hiking"
+#  else
+#  	puts "I'M staying in and coding"
+#  end
+
